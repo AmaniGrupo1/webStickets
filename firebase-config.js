0834931aa31a97bf3fc0f9464808920567ce78ea
Felix12232 <felixpa2001@gmail.com>
2026-05-25 18:03:46 +0200

    Tercera Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,24 @@
 // Configuración de Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getDatabase, ref, push, set, onValue, update, remove, get } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
-import { getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { 
+    getFirestore, 
+    collection, 
+    doc, 
+    addDoc, 
+    updateDoc, 
+    deleteDoc, 
+    getDocs, 
+    getDoc, 
+    onSnapshot,
+    query,
+    orderBy
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 // Tu configuración de Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyAwQTSzKnxsqKHfTTfLYWzKnWdHxFo1YUU",
     authDomain: "amani-160bf.firebaseapp.com",
-    databaseURL: "https://amani-160bf-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "amani-160bf",
     storageBucket: "amani-160bf.firebasestorage.app",
     messagingSenderId: "560473593904",
@@ -19,28 +28,24 @@ const firebaseConfig = {
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getDatabase(app);
+const db = getFirestore(app);
 const auth = getAuth(app);
-const dbFirestore = getFirestore(app);
 
-// Exportar todo para usar en otros archivos
+// Exportar todo
 export { 
     db, 
-    dbFirestore,
-    collection,
-    addDoc,
     auth, 
-    ref, 
-    push, 
-    set, 
-    onValue, 
-    update, 
-    remove, 
-    get,
-    GoogleAuthProvider,
+    collection,
+    doc,
+    addDoc,
+    updateDoc,
+    deleteDoc,
+    getDocs,
+    getDoc,
+    onSnapshot,
+    query,
+    orderBy,
     signInWithEmailAndPassword,
-    signInWithPopup,
     onAuthStateChanged,
     signOut
 };
